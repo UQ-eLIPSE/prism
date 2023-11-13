47cba680f8b544cd0d411dc3977cb2a0f23d0acb
Joanna Zhong <joanna.zhong@uq.edu.au>
2023-11-13 12:54:03 +1000

    fix comment issues

--- atdd/README.md
+++
@@ -1,6 +0,0 @@
-**Prism ATDD TEST**
-_Quick Start_
-
-1. navigate to test folder: `cd prism/atdd/`
-2. install packages: `yarn`
-3. start tests: `yarn cypress:run`
--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  env: {
+    username: process.env.CYPRESS_USERNAME,
+    password: process.env.CYPRESS_PASSWORD,
+  },
   e2e: {
     // Your project's end-to-end testing configuration
-    setupNodeEvents(on, config) {
-      // handle events here
-    },
-    specPattern: "e2e/**/*.cy.{js,ts}",
-    // Adjust to your application's URL
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
     // baseUrl: "http://localhost:3000",
-    baseUrl: "https://prism-020.uqcloud.net",
-    // additional configurations...
+    baseUrl: "https://prism-020.uqcloud.net", // Adjust to your application's URL
+    //additional configurations...
     supportFile: false,
+    screenshotOnRunFailure: false,
   },
   // Add any plugins you are using
   component: {
