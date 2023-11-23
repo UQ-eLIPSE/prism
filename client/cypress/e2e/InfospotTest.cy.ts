145324f227d42257193f619363b9b2ea3f505be0
Jo-Zh <joanna.zhong@uq.edu.au>
2023-11-24 09:28:30 +1000

    fix comments issues

--- client/cypress/e2e/InfospotTest.cy.ts
+++ client/cypress/e2e/InfospotTest.cy.ts
@@ -1,4 +1,5 @@
-import { testEachZone } from "../testutils";
+import { testEachZone, performChecks } from "../testutils";
+
 testEachZone((zone: Cypress.PrismZone) => {
   describe("Test case: InforLinknode in Expanded Left Top Bar should match linkNode in 3D view", () => {
     beforeEach(() => {
@@ -7,42 +8,7 @@ testEachZone((zone: Cypress.PrismZone) => {
 
     it(`Testing in each Zone: should compare elements with .hotspot-tooltip and .linkNodeNames`, () => {
       if (zone.hotspots) {
-        cy.get(".hotspot.link-hotspot")
-          .filter((index, element) => {
-            // Filter those elements based on a condition
-            const $element = Cypress.$(element);
-            const grandparent = $element.parent().parent();
-            return grandparent.css("display") === "block";
-          })
-          .should("exist")
-          .find(".hotspot-tooltip")
-          .then(($collection1) => {
-            const htmlCollection1 = $collection1
-              .map((index, html) => html.innerHTML)
-              .get();
-            cy.get(".linkButton").should("exist").click();
-            cy.get(".linkNodeNames")
-              .should("exist")
-              .then(($collection2) => {
-                const htmlCollection2 = $collection2
-                  .map((index, html) => html.textContent ?? "")
-                  .get();
-
-                // Test: Comparing the length of both collections
-                expect(
-                  htmlCollection1.length,
-                  "Both collections should have the same number of elements"
-                ).to.equal(htmlCollection2.length);
-
-                // Test: Continue with comparison content
-                htmlCollection1.forEach((html, index) => {
-                  expect(html.trim()).to.equal(
-                    htmlCollection2[index].trim(),
-                    `HTML content of element ${index} should match`
-                  );
-                });
-              });
-          });
+        performChecks();
       }
     });
 
@@ -57,62 +23,8 @@ testEachZone((zone: Cypress.PrismZone) => {
               }
             });
         });
-
-        function performChecks() {
-          cy.get(".hotspot.link-hotspot").then(
-            ($result: JQuery<HTMLElement>) => {
-                const filtered = $result.filter((index, element) => {
-                  const $element = Cypress.$(element);
-                  const grandparent = $element.parent().parent();
-                  return grandparent.css("display") === "block";
-                });
-
-                if (filtered.length <= 0) {
-                  return;
-                } else {
-                  cy.wrap(filtered)
-                    .find(".hotspot-tooltip")
-                    .then(($collection1: JQuery<HTMLElement>) => {
-                      const htmlCollection1 = $collection1
-                        .map((index, html) => html.innerHTML)
-                        .get();
-                      cy.get(".linkButton")
-                        .should("exist")
-                        .then(($linkButton) => {
-                          if (
-                            $linkButton.find(".fas.fa-chevron-up").length === 0
-                          ) {
-                            cy.wrap($linkButton).click();
-                          } else {
-                            cy.log(
-                              "The .linkButton contains the .fas.fa-chevron-up icon, not clicking."
-                            );
-                          }
-                        });
-
-                      cy.get(".linkNodeNames")
-                        .should("exist")
-                        .then(($collection2) => {
-                          const htmlCollection2 = $collection2
-                            .map((index, html) => html.textContent ?? "")
-                            .get();
-                          expect(
-                            htmlCollection1.length,
-                            "Both collections should have the same number of elements"
-                          ).to.equal(htmlCollection2.length);
-                          htmlCollection1.forEach((html, index) => {
-                            expect(html.trim()).to.equal(
-                              htmlCollection2[index].trim(),
-                              `HTML content of element ${index} should match`
-                            );
-                          });
-                        });
-                    });
-                }
-            }
-          );
-        }
       });
     });
   });
 });
+
--- client/cypress/testutils.ts
+++ client/cypress/testutils.ts
@@ -7,4 +7,60 @@ const testEachZone = (testFn: (zone: Cypress.PrismZone) => void): void => {
   });
 };
 
-export { testEachZone };
+const performChecks = () => {
+  cy.get(".hotspot.link-hotspot").then(
+    ($result: JQuery<HTMLElement>) => {
+        const filtered = $result.filter((index, element) => {
+          const $element = Cypress.$(element);
+          const grandparent = $element.parent().parent();
+          return grandparent.css("display") === "block";
+        });
+
+        if (filtered.length <= 0) {
+          return;
+        } else {
+          cy.wrap(filtered)
+            .find(".hotspot-tooltip")
+            .then(($collection1: JQuery<HTMLElement>) => {
+              const htmlCollection1 = $collection1
+                .map((index, html) => html.innerHTML)
+                .get();
+              cy.get(".linkButton")
+                .should("exist")
+                .then(($linkButton) => {
+                  if (
+                    $linkButton.find(".fas.fa-chevron-up").length === 0
+                  ) {
+                    cy.wrap($linkButton).click();
+                  } else {
+                    cy.log(
+                      "The .linkButton contains the .fas.fa-chevron-up icon, not clicking."
+                    );
+                  }
+                });
+
+              cy.get(".linkNodeNames")
+                .should("exist")
+                .then(($collection2) => {
+                  const htmlCollection2 = $collection2
+                    .map((index, html) => html.textContent ?? "")
+                    .get();
+                  expect(
+                    htmlCollection1.length,
+                    "Both collections should have the same number of elements"
+                  ).to.equal(htmlCollection2.length);
+                  htmlCollection1.forEach((html, index) => {
+                    expect(html.trim()).to.equal(
+                      htmlCollection2[index].trim(),
+                      `HTML content of element ${index} should match`
+                    );
+                  });
+                });
+            });
+        }
+    }
+  );
+}
+
+export { testEachZone, performChecks };
+
