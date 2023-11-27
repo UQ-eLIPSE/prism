532ad1141fc20b576a9b2656fcbab1ba8def9508
Joanna Zhong <joanna.zhong@uq.edu.au>
2023-11-27 16:25:46 +1000

    fix comments issues

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,11 +1,8 @@
 Cypress.Commands.add("accessZone", (zone: Cypress.PrismZone): void => {
   if (zone.singleSite) {
     cy.visit(`${zone.url}/site`);
-    cy.wait(1000);
   } else {
     cy.visit(zone.url);
-    // TODO: aiming remove all cy.wait, at this stage it is necessary to keep it
-    cy.wait(1000);
     cy.get(".pin.enabled.enabled.false.bottom.enabled").click({
       force: true,
     });
