f3b455ad61c308470de37cd6f79541360ab61898
Joanna Zhong <joanna.zhong@uq.edu.au>
2023-11-28 08:41:01 +1000

    update Sce.Test with excl.zones.hotspot=false, update package.json in server, lint

--- client/cypress/e2e/ScenenameTest.cy.ts
+++ client/cypress/e2e/ScenenameTest.cy.ts
@@ -6,21 +6,23 @@ testEachZone((zone: Cypress.PrismZone) => {
       cy.accessZone(zone);
     });
     it(`Testing:  Scene Name Displaying in div .title-card`, () => {
-      cy.get("[class^='_minimap_closeShow']").should("exist").click();
-      cy.get("[data-cy=selected-node]")
-        .should("exist")
-        .parent("[class^='_minimap_nodeContainer']")
-        .should("exist")
-        .parent()
-        .children("div")
-        .eq(1)
-        .should("exist")
-        .invoke("text")
-        .then((text) => {
-          cy.get("[data-cy='facility-name-title']")
-            .invoke("text")
-            .should("eq", text.trim());
-        });
+      if (zone.hotspots) {
+        cy.get("[class^='_minimap_closeShow']").should("exist").click();
+        cy.get("[data-cy=selected-node]")
+          .should("exist")
+          .parent("[class^='_minimap_nodeContainer']")
+          .should("exist")
+          .parent()
+          .children("div")
+          .eq(1)
+          .should("exist")
+          .invoke("text")
+          .then((text) => {
+            cy.get("[data-cy='facility-name-title']")
+              .invoke("text")
+              .should("eq", text.trim());
+          });
+      }
     });
   });
 });
