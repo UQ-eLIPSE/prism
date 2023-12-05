1383fcdfe966ee18f64407008e94c456a52634cf
Jo-Zh <joanna.zhong@uq.edu.au>
2023-12-06 08:54:08 +1000

    create environment variable JS

---
+++ client/env_agco.js
@@ -0,0 +1,4 @@
+window._env_ = {
+    API_URL: "https://prism-019.uqcloud.net",
+    PROJECT_TITLE: "PRISM",
+    }
---
+++ client/env_uw.js
@@ -0,0 +1,4 @@
+window._env_ = {
+    API_URL: "https://prism-020.uqcloud.net",
+    PROJECT_TITLE: "PRISM",
+    }
