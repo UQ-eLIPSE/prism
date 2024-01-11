0b7b7a86d052a5e63db2c7f43fafcc28c49d67ef
ramonvega96 <ramonvega150696@gmail.com>
2024-01-11 17:40:27 +1000

    Bugfix applied to minimap update

---
+++ client/cypress/e2e/MinimapUpdateTest.ts
@@ -0,0 +1,40 @@
+import { testEachZone } from "../testutils";
+
+testEachZone((zone: Cypress.PrismZone) => {
+  describe("Test case: Minimap tag, floor name and image update should work", () => {
+    beforeEach(() => {
+      cy.accessZone(zone);
+    });
+    it(`Testing: Minimap Floor Name update works`, () => {
+      cy.intercept("PATCH", "/api/site/*/sitemap").as("MinimapFloorNamePatch");
+      cy.get('[data-cy="floor-name-input"]').should("exist").click();
+      cy.get('[data-cy="floor-name-input"]').type(" - test");
+      cy.get("[class^='submit-update']").should("exist").click();
+      cy.wait("@MinimapFloorNamePatch")
+        .its("response.statusCode")
+        .should("equal", 200);
+    });
+    it(`Testing: Minimap Floor Tag update works`, () => {
+      cy.intercept("PATCH", "/api/site/*/sitemap").as("MinimapFloorTagPatch");
+      cy.get('[data-cy="floor-tag-input"]').should("exist").click();
+      cy.get('[data-cy="floor-tag-input"]').type("0");
+      cy.get("[class^='submit-update']").should("exist").click();
+      cy.wait("@MinimapFloorTagPatch")
+        .its("response.statusCode")
+        .should("equal", 200);
+    });
+    it(`Testing: Minimap Image update works`, () => {
+      cy.intercept("POST", "/api/site/*/minimap?floor=*").as(
+        "MinimapImagePost",
+      );
+      cy.get('[id="select-image"]').selectFile(
+        "cypress/fixtures/minimap_test.png",
+        { force: true },
+      );
+      cy.get("[class^='submit-update']").should("exist").click();
+      cy.wait("@MinimapImagePost")
+        .its("response.statusCode")
+        .should("equal", 200);
+    });
+  });
+});
