ac8939768a3e02608b87d34a593d52ec6380f755
jtsou <uqjtsou@uq.edu.au>
2024-01-25 12:55:27 +1000

    Add interface of EditNodeInput

---
+++ client/src/interfaces/MiniMap/EditNodeInput.ts
@@ -0,0 +1,13 @@
+/**
+ * An interface to represent each input field in the EditNode component.
+ * Used under ./src/components/EditNodeForm.tsx
+ * @interface EditNodeInput
+ * @property {string} label - The id and name of the input field.
+ * @property {number} value - The value of the input field.
+ * @property {function} setValue - A function to set the value of the input field.
+ */
+export interface EditNodeInput {
+  label: string;
+  value: number;
+  setValue: (value: number) => void;
+}
