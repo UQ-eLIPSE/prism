6cb3eef1a1d8cee3a5e178eb70e13ef76b573c3d
Mohamad <moh.dabboussi@gmail.com>
2024-01-30 14:47:49 +1000

    Added some cypress tests for rotation coordinate input for two different save buttons

--- client/cypress/e2e/MinimapTest.cy.ts
+++ client/cypress/e2e/MinimapTest.cy.ts
@@ -2,6 +2,43 @@
 
 import { testEachZone } from "../testutils";
 
+function editNode() {
+  cy.get('i[class*="fa-expand-arrows-alt"]').click({
+    timeout: 10000,
+    force: true,
+  });
+  cy.get("p")
+    .contains("Edit Node")
+    .should("exist")
+    .click({ force: true });
+  cy.get("h2").contains("Select a Node to Edit");
+}
+
+function typeRotation(rotation:number) {
+  cy.get("input[id='orientation']").should("exist").clear();
+  cy.get("input[id='orientation']")
+    .should("exist")
+    .type(String(rotation));
+}
+
+function checkStyleContains(styleString: string) {
+  cy.get("[data-cy='selected-node']")
+    .parent()
+    .should("have.attr", "style")
+    .should("contain", styleString);
+}
+
+const rotationValues = [
+  { degrees: 30, cssValue: "rotate(0.523599rad)" }, // degrees / 57.2958 = rad
+  { degrees: 60, cssValue: "rotate(1.0472rad)" },
+  { degrees: 90, cssValue: "rotate(1.5708rad)" },
+  { degrees: 120, cssValue: "rotate(2.09439rad)"},
+  { degrees: 150, cssValue: "rotate(2.61799rad)"},
+  { degrees: 180, cssValue: "rotate(3.14159rad)"},
+  { degrees: 210, cssValue: "rotate(3.66519rad)"},
+  { degrees: 240, cssValue: "rotate(4.18879rad)"},
+];
+
 testEachZone((zone: Cypress.PrismZone) => {
   describe("Test case: When user selected a mininode on minimap, should be able to update coordinates via input form", () => {
     beforeEach(() => {
@@ -50,53 +87,57 @@ testEachZone((zone: Cypress.PrismZone) => {
       }
     });
 
-    it.only(`Testing: user changes rotation coordinate input in the form, the targeted mininode rotation changes correctly`, () => {
+    it.only(`Testing: user changes rotation coordinate input in the form, the targeted mininode rotation changes correctly when pressing the "Save node" button`, () => {
       if (zone.adminUser) {
         cy.intercept("PATCH", "/api/node/coords/*").as("patchNodeCoords");
         cy.intercept("PATCH", "/api/node/rotation/*").as("patchNodeRoration");
         cy.intercept("GET", "/api/site/*/*/survey/minimapSingleSite*").as(
           "getMinimapData",
         );
-        cy.get('i[class*="fa-expand-arrows-alt"]').click({
-          timeout: 10000,
-          force: true,
-        });
-        cy.get("p")
-          .contains("Edit Node")
-          .should("exist")
-          .click({ force: true });
-        cy.get("h2").contains("Select a Node to Edit");
 
-        const values = [
-          [30, "rotate(0.523599rad)"],
-          [60, "rotate(1.0472rad)"],
-          [90, "rotate(1.5708rad)"],
-        ];
+        editNode();
 
-        const randTuple = values[Math.floor(Math.random() * values.length)];
-        const randOrientation = randTuple[0];
+        const randTuple = rotationValues[Math.floor(Math.random() * rotationValues.length)];
 
         cy.get("[data-cy='selected-node']").click({ force: true });
         cy.wait("@getMinimapData").then(() => {
-          cy.get("input[id='orientation']").should("exist").clear();
-          cy.get("input[id='orientation']")
-            .should("exist")
-            .type(String(randOrientation));
-          cy.get("button").contains("Save").click();
+          typeRotation(randTuple.degrees);
+          cy.get("[data-cy='edit-save-button']").contains("Save").click();
           cy.wait("@patchNodeCoords")
             .wait("@patchNodeRoration")
             .wait("@getMinimapData")
             .then(() => {
-              cy.get("[data-cy='selected-node']")
-                .parent()
-                .should("have.attr", "style")
-                .should("contain", `transform: ${randTuple[1]}`, {
-                  timeout: 10000,
-                  retryInterval: 1000,
-                });
+              checkStyleContains(`transform: ${randTuple.cssValue}`);
             });
         });
       }
     });
+
+    it.only(`Testing: user changes rotation coordinate input in the form, the targeted mininode rotation changes correctly when pressing the "Save" button`, () => {
+      if (zone.adminUser) {
+        cy.intercept("PATCH", "/api/node/coords/*").as("patchNodeCoords");
+        cy.intercept("PATCH", "/api/node/rotation/*").as("patchNodeRoration");
+        cy.intercept("GET", "/api/site/*/*/survey/minimapSingleSite*").as(
+          "getMinimapData",
+        );
+
+        editNode();
+
+        const randTuple = rotationValues[Math.floor(Math.random() * rotationValues.length)];
+
+        cy.get("[data-cy='selected-node']").click({ force: true });
+        cy.wait("@getMinimapData").then(() => {
+          typeRotation(randTuple.degrees);
+          cy.get("[data-cy='submit-button']").contains("Save").click();
+          cy.wait("@patchNodeCoords")
+            .wait("@patchNodeRoration")
+            .wait("@getMinimapData")
+            .then(() => {
+              checkStyleContains(`transform: ${randTuple.cssValue}`);
+            });
+        });
+      }
+    });
+
   });
 });
