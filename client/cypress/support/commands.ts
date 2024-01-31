27bb677b6b4661c1abe54e72a0f0155215dd5a56
Jo-Zh <joanna.zhong@uq.edu.au>
2024-01-31 11:32:16 +1000

    fix comments issues

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -2,20 +2,10 @@ Cypress.Commands.add("accessZone", (zone: Cypress.PrismZone): void => {
   const test = Cypress.env("testurl");
   if (zone.singleSite) {
     cy.visit(
-      test === "local"
-        ? `${zone.url.local}/site`
-        : test === "UAT"
-          ? `${zone.url.uat}/site`
-          : `${zone.url.product}/site`,
+      test === "local" ? `${zone.url.local}/site` : `${zone.url.uat}/site`,
     );
   } else {
-    cy.visit(
-      test === "local"
-        ? `${zone.url.local}`
-        : test === "UAT"
-          ? zone.url.uat
-          : zone.url.product,
-    );
+    cy.visit(test === "local" ? zone.url.local : zone.url.uat);
     cy.get(".pin.enabled.enabled.false.bottom.enabled").click({
       force: true,
     });
