b7764319704d5a977a68fab533b29aa76780d308
jtsou <121275444+freshc0w@users.noreply.github.com>
2024-02-01 14:53:02 +1000

    Add cypress testing on visibility of timeline

--- client/cypress/e2e/TimelineUITest.cy.ts
+++ client/cypress/e2e/TimelineUITest.cy.ts
@@ -26,4 +26,64 @@ testEachZone((zone: Cypress.PrismZone) => {
       }
     });
   });
+
+  describe(`Test case: Visibility toggle of Timeline section UI`, () => {
+    beforeEach(() => {
+      cy.accessZone(zone);
+      cy.intercept("GET", "/api/site/*/*/exists").as("getSiteExists");
+      cy.intercept("GET", "/api/site/*/survey/details/*").as("getSiteDetails");
+      cy.intercept("GET", "/api/site/*/emptyFloors").as("getEmptyFloors");
+    });
+
+    const checkTransformStyle = (
+      element: JQuery<HTMLElement>,
+      shouldBeNone: boolean,
+    ) => {
+      const style = window.getComputedStyle(element[0]);
+      const { transform } = style;
+      shouldBeNone
+        ? expect(transform).to.be.equal("none")
+        : expect(transform).to.not.be.equal("none");
+    };
+
+    it(`Testing: Timeline drawer should be hidden in initial page render`, () => {
+      cy.wait("@getSiteExists").then(() => {
+        cy.wait("@getSiteDetails").then(() => {
+          cy.wait("@getEmptyFloors").then(() => {
+            cy.get("#drawer-container")
+              .should("exist")
+              .parent()
+              .then(($parent) => {
+                checkTransformStyle($parent, false);
+              });
+          });
+        });
+      });
+    });
+
+    it(`Testing: Timeline drawer toggle visibility after clicking timeline button action`, () => {
+      if (zone.timeline) {
+        cy.wait("@getSiteExists").then(() => {
+          cy.wait("@getSiteDetails").then(() => {
+            cy.wait("@getEmptyFloors").then(() => {
+              cy.get("[class^='_timelineButton']").click({ force: true });
+              cy.get("#drawer-container")
+                .should("exist")
+                .parent()
+                .then(($parent) => {
+                  checkTransformStyle($parent, true);
+                  cy.get("[class^='_timelineButton']").click({ force: true });
+                  cy.get("#drawer-container")
+                    .should("exist")
+                    .parent()
+                    .then(($parent) => {
+                      checkTransformStyle($parent, false);
+                    });
+                });
+            });
+          });
+        });
+      }
+    });
+  });
 });
