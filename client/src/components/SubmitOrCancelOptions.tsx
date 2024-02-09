625b1c9474254e54ca258fc34493bc5e56db1ccf
jtsou <121275444+freshc0w@users.noreply.github.com>
2024-02-09 16:45:08 +1000

    Update render condition in SubmitOrCancelOptions component

--- client/src/components/SubmitOrCancelOptions.tsx
+++ client/src/components/SubmitOrCancelOptions.tsx
@@ -8,7 +8,7 @@ interface SubmitOrCancelOptionsProps {
 
 /**
  * A component that displays Submit and Cancel options
- * Renders if showCondition prop is not provided.
+ * Renders even if showCondition prop is not provided.
  * @returns {JSX.Element} Submit or Cancel options
  */
 const SubmitOrCancelOptions = ({
