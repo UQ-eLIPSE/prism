d92a8e9de545e679032787fd73414a6e43212736
jtsou <121275444+freshc0w@users.noreply.github.com>
2024-02-09 16:36:23 +1000

    Update handleSubmit function signature in SubmitOrCancelOptions component

--- client/src/components/SubmitOrCancelOptions.tsx
+++ client/src/components/SubmitOrCancelOptions.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface SubmitOrCancelOptionsProps {
   showCondition?: boolean;
-  handleSubmit: () => void;
+  handleSubmit: () => void | Promise<void>;
   handleCancel: () => void;
 }
 
