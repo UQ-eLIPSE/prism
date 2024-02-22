efc46183221366f8d7df8b7af2b9881388fe8aba
Chia-Chuan Tsou (Jason) <121275444+freshc0w@users.noreply.github.com>
2024-02-22 10:04:24 +1000

    feature/TEAM24-159 (#50)
    * Add working version of orientation save
    
    * Refactor Marzipano integration and update view parameters
    
    Remove all unecessary logs as well.
    
    * Add testing to marzipano
    
    * Fix testing and update marzipano.ts
    
    Marzipano now has an additional constructor initialRender that dicates if it should delay the view change event listener
    
    * Add comments to marzipano.ts
    
    * Refactor MarzipanoTest.cy.ts for improved readability and maintainability
    
    * Remove console.log statements and update comments
    
    * Run lint:pretty
    
    * Refactor MarzipanoTest.cy.ts to skip tests when zone has no floors

--- client/src/components/Site.tsx
+++ client/src/components/Site.tsx
@@ -81,9 +81,13 @@ function Site(props: SiteInterface) {
   );
   const [currRotation, setCurrRotation] = useState<number>(0);
 
+  const [lastRotation, setLastRotation] = useState<number>(0);
+
   const [currViewParams, setCurrViewParams] = useState<InitialViewParameters>({
     ...config.initial_settings,
   });
+  const [lastViewParams, setLastViewParams] =
+    useState<InitialViewParameters | null>(null);
 
   const [currfloor, setCurrFloor] = useState<number>(
     config.initial_settings.floor,
@@ -109,6 +113,9 @@ function Site(props: SiteInterface) {
   const [floorTag, setFloorTag] = useState<string>("");
   const [availableFloors, setAvailableFloors] = useState<number[]>([0]);
 
+  // Used to detect if page is being rendered for the first time (not switching floors).
+  const [initialRender, setInitialRender] = useState<boolean>(true);
+
   useEffect(() => {
     if (marzipano.current) {
       marzipano.current = undefined;
@@ -124,7 +131,6 @@ function Site(props: SiteInterface) {
 
       // Update correspondingly floors based on prior async data.
       updateFloor(currfloor);
-
       await updateFloors();
     })();
   }, [currfloor, currDate, floorExists]);
@@ -136,6 +142,17 @@ function Site(props: SiteInterface) {
       await updateSurveys();
     })();
   }, []);
+
+  useEffect(() => {
+    if (!lastViewParams) return;
+    if (marzipano.current && marzipano.current.findSceneById(currPanoId)) {
+      marzipano.current.updateCurrView(
+        lastViewParams,
+        marzipano.current.findSceneById(currPanoId),
+      );
+      marzipano.current.updateRotation(lastRotation);
+    }
+  }, [currPanoId]);
 
   const updateFloors = async (): Promise<void> => {
     try {
@@ -215,7 +232,7 @@ function Site(props: SiteInterface) {
   }
 
   function updateCurrPano(panoId: string): void {
-    setCurrPanoId(panoId);
+    setCurrPanoId(() => panoId);
     getHotspotDescriptionList(panoId);
   }
 
@@ -237,22 +254,29 @@ function Site(props: SiteInterface) {
       viewParams,
       marzipano.current.findSceneById(currPanoId),
     );
+
     updateViewParams(viewParams);
     // Open info panel
     getInfoHotspot(info_id);
   }
 
   function minimapClick(panoId: string): void {
-    if (marzipano.current)
+    if (marzipano.current) {
       marzipano?.current?.switchScene(marzipano.current.findSceneById(panoId));
+      setLastViewParams(() => null);
+      setLastRotation(() => 0);
+    }
   }
 
   function updateRotation(rotation: number): void {
     setCurrRotation(rotation);
+    setLastRotation(() => rotation);
   }
 
   function updateViewParams(viewParams: InitialViewParameters): void {
+    if (viewParams === currViewParams) return;
     setCurrViewParams(viewParams);
+    setLastViewParams(() => viewParams);
   }
 
   function getSurveyNodes(floor: number = 0): void {
@@ -292,7 +316,9 @@ function Site(props: SiteInterface) {
               updateViewParams,
               changeInfoPanelOpen,
               config,
+              initialRender,
             );
+            setInitialRender(false);
           }
           // Get current tile id based on previous node number
           let currentTilesId = nodesData[0].minimap_node.tiles_id;
@@ -324,7 +350,8 @@ function Site(props: SiteInterface) {
               ? currPanoId
               : currentTilesId,
           );
-
+          setLastViewParams(() => viewParams);
+          setLastRotation(() => currRotation);
           if (
             viewParams.fov !== 0 ||
             viewParams.pitch !== 0 ||
