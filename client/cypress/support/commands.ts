a2ec4eb6e9cc45d3d33086cd70f7707d466195f4
Joanna Zhong <joanna.zhong@uq.edu.au>
2024-02-26 16:25:20 +1000

    update cypress command and config

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,15 +1,22 @@
 Cypress.Commands.add("accessZone", (zone: Cypress.PrismZone): void => {
   const test = Cypress.env("testurl");
+  console.log(test);
   if (zone.singleSite) {
     cy.visit(
       test === "local" ? `${zone.url.local}/site` : `${zone.url.uat}/site`,
     );
   } else {
+    console.log(test);
+    cy.log(test);
     cy.visit(test === "local" ? zone.url.local : zone.url.uat);
-    // TODO: to discuss whether implement choose any/all map-pin needed or choose a default one can be satisfied for the test
-    cy.get(".pin.enabled.enabled.false.bottom.enabled").eq(0).click({
-      force: true,
-    });
+    // TODO: to discuss whether implement choose all map-pin needed or choose a random one can be satisfied for the test
+    cy.get(".pin.enabled.enabled.false.bottom.enabled")
+      .should("have.length.gte", 1)
+      .then(($div) => {
+        const items = $div.toArray();
+        return Cypress._.sample(items);
+      })
+      .click({ force: true });
     if (zone.adminUser) {
       cy.contains("Go to Site").click({
         force: true,
