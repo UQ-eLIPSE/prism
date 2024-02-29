60d0ea3d99caf38bb39df454c4fca4c8eddb85c5
Joanna <93627519+Jo-Zh@users.noreply.github.com>
2024-02-29 14:45:11 +1000

    feature/TEAM24-225 (#74)
    * create minimapConversion dal function
    
    * update minimap conversion dal
    
    * add minimapConversionHandler
    
    * change handler name
    
    * change names
    
    * change names
    
    * updates
    
    * add test for  getIndividualSurveysDetails
    
    * bugfix/TEAM24-197 (#64)
    
    * Fix black arrow consistency
    
    * Remove comments
    
    * Fix red arrow consistency
    
    * update comment
    
    * Add cypress testing
    
    * Run yarn:lint:pretty
    
    * Fix edit node click sometimes not triggering
    
    * Remove visibility check for black yaw arrow
    
    ---------
    
    Co-authored-by: Mohamad <moh.dabboussi@gmail.com>
    
    * bugfix/TEAM24-193 (#71)
    
    * Fix server jest test
    
    * Remove logs
    
    * bugfix/TEAM24-222 (#70)
    
    * Add counter rotation style to NodeComponent
    
    * Refine regex for counterotatestyle and add jest testing
    
    * Refactor test description for getCounterRotationStyle function
    
    * Remove log and dummy test file
    
    * fix comments
    
    * fix comment
    
    ---------
    
    Co-authored-by: Chia-Chuan Tsou (Jason) <121275444+freshc0w@users.noreply.github.com>
    Co-authored-by: Mohamad <moh.dabboussi@gmail.com>

--- server/src/dal/minimapConversionsHandler.ts
+++ server/src/dal/minimapConversionsHandler.ts
@@ -1,13 +1,33 @@
 import { ObjectId, DeleteResult } from "mongodb";
 import { MinimapConversion, IMinimapConversion } from "../models/SurveyModel";
+//TODO: remove any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function typeGuardMiniConversion(obj: any): obj is IMinimapConversion {
+  console.log(obj);
+  return (
+    typeof obj.floor === "number" &&
+    typeof obj.x === "number" &&
+    typeof obj.y === "number" &&
+    typeof obj.x_scale === "number" &&
+    typeof obj.y_scale === "number" &&
+    typeof obj.rotation === "number"
+  );
+}
 
 export const findByFloorAndSite = async (
   floor: string,
   siteId: string,
 ): Promise<IMinimapConversion[]> => {
-  return MinimapConversion.find({ floor, site: new ObjectId(siteId) }, "-_id")
+  const result: IMinimapConversion[] = await MinimapConversion.find(
+    { floor, site: new ObjectId(siteId) },
+    "-_id",
+  )
     .populate("survey_node", "-_id")
     .populate("minimap_node", "-_id");
+  result.forEach((item) => {
+    console.log(typeGuardMiniConversion(item));
+  });
+  return result;
 };
 
 export const findOneBySurveyNodeWithRelated = async (
@@ -47,4 +67,16 @@ export const deleteOneMinimapCvs = async (
   return MinimapConversion.deleteOne({
     survey_node: new ObjectId(surveyNodeId),
   });
+};
+
+export const minimapConversionCreateOne = async (
+  objectMini: IMinimapConversion,
+): Promise<IMinimapConversion> => {
+  return MinimapConversion.create(objectMini);
+};
+
+export const createMinimapConversion = async (
+  data: IMinimapConversion,
+): Promise<IMinimapConversion> => {
+  return MinimapConversion.create(data);
 };
